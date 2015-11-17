a95522dc0cd9ef46a0dddce617a283090d58a359
Michael Toth <michael@virtualpianist.com>
2015-11-17 08:44:40 -0600

    page 3

--- maze.py
+++ maze.py
@@ -1,11 +1,16 @@
 # tdd maze by Michael Toth
 
+setMediaPath('/Users/michaeltoth/Documents/tddmaze')
 class Maze:
   """ A class which can solve a maze given by maze.jpg """
-  pass
+  def __init__(self):
+    self.image=makePicture('maze.jpg')
   
 # tests
 
 if 1:
     # test for existance of maze
     m = Maze()
+    # test for image
+    assert m.image.__class__==Picture
+    
